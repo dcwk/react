ef337f04efe82e857798ece6f376b4c67a799159
Ruslan Golovizin <dcwk@yandex.ru>
2024-06-28 23:03:29 +0300

    Функции обработчиков вынесены в константы

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
-ReactDOM.render(React.createElement('input', {
-        placeholder: "Help text",
-        onClick: () => console.log("Clicked"),
-        onMouseEnter: () => console.log("Mouse over"),
-    }),
-    document.getElementById("app")
-)
+// ReactDOM.render(React.createElement('input', {
+//         placeholder: "Help text",
+//         onClick: () => console.log("Clicked"),
+//         onMouseEnter: () => console.log("Mouse over"),
+//     }),
+//     document.getElementById("app")
+// )
+
+const inputClick = () => console.log("Clicked")
+const mouseOver = ()=> console.log("Mouse Over")
+
+const helpText = "Help text!"
+const elements = (<div className="name">
+    <input placeholder={helpText}
+           onClick={inputClick} onMouseOver={mouseOver}/>
+    <p>{helpText == "Help text" ? "Yes" : "No"}</p>
+</div>)
+const app = document.getElementById("app")
+
+ReactDOM.render(elements, app)
